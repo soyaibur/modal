929d651a71d38b19400abd8d52810f5b60d249a2
soyaibur <ovisoyaiburrahman@gmail.com>
2021-12-02 21:21:15 +0600

    initial js added

--- logic.js
+++ logic.js
@@ -5,6 +5,7 @@
 
 // .....All Element Selection will be undernith here.......
 const openModal = document.querySelectorAll('[data-modal-set]')
+const closeModal = document.querySelectorAll('[data-modal-close]')
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
@@ -12,7 +13,17 @@ const openModal = document.querySelectorAll('[data-modal-set]')
 // .....All EventListener go undernith here...........
 openModal.forEach(modal=>{
     modal.addEventListener('click',()=>{
-        
+        const modalOp = document.querySelector(modal.dataset.modalSet)
+        if (modalOp == null){return modalOp}
+        if(modalOp){
+            modalOp.classList.add('active')
+        }
+    })
+})
+closeModal.forEach(closeBtn =>{
+    closeBtn.addEventListener('click',()=>{
+         closeBtn.closest('.modal').classList.remove('active')
+         
     })
 })
 // :::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -21,6 +32,16 @@ openModal.forEach(modal=>{
 
 // ......All Function will go here undernith...............
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+
+
+
+
+
+
+
 
 //All code will be deleted here.............
 function printOut(text){
