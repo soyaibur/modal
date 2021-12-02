a2550dacfaafc314e284c8e88f44dd888cc7b56c
soyaibur <ovisoyaiburrahman@gmail.com>
2021-12-02 10:39:06 +0600

    button selection and Eventlistener added

--- logic.js
+++ logic.js
@@ -1,19 +1,25 @@
 // ....Univesal Stracture of function and others......
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// :::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 
-// .....All Element Selection will be undernith here...........
+// .....All Element Selection will be undernith here.......
+const openModal = document.querySelectorAll('[data-modal-set]')
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 
 // .....All EventListener go undernith here...........
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+openModal.forEach(modal=>{
+    modal.addEventListener('click',()=>{
+        
+    })
+})
+// :::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 
-// ......All Function will go here undernith..........
+// ......All Function will go here undernith...............
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 //All code will be deleted here.............
