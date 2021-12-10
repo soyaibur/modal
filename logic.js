9b13d263c46a55a6a15e54ec51214b96d59db277
soyaibur <ovisoyaiburrahman@gmail.com>
2021-12-10 12:45:25 +0600

    completed project

--- logic.js
+++ logic.js
@@ -5,25 +5,32 @@
 
 // .....All Element Selection will be undernith here.......
 const openModal = document.querySelectorAll('[data-modal-set]')
-const closeModal = document.querySelectorAll('[data-modal-close]')
+const closeBtn = document.querySelectorAll('[data-modal-close]')
+const overlay = document.getElementById('overlay')
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 
 // .....All EventListener go undernith here...........
-openModal.forEach(modal=>{
-    modal.addEventListener('click',()=>{
-        const modalOp = document.querySelector(modal.dataset.modalSet)
-        if (modalOp == null){return modalOp}
-        if(modalOp){
-            modalOp.classList.add('active')
-        }
+overlay.addEventListener('click',()=>{
+    const modal = document.querySelectorAll('.modal.active')
+    modal.forEach(mod=>{
+        mod.classList.remove('active')
+        overlay.classList.remove('active')
     })
 })
-closeModal.forEach(closeBtn =>{
-    closeBtn.addEventListener('click',()=>{
-         closeBtn.closest('.modal').classList.remove('active')
-         
+openModal.forEach(Omodal=>{
+    Omodal.addEventListener('click',()=>{
+        if(Omodal == null) return
+        const aModal = document.querySelector(Omodal.dataset.modalSet)
+        activeModal(aModal)
+    })
+})
+closeBtn.forEach(singleBtn=>{
+    singleBtn.addEventListener('click',()=>{
+        const cBtn = singleBtn.closest('.modal.active')
+        clossModal(cBtn)
+        
     })
 })
 // :::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -31,25 +38,12 @@ closeModal.forEach(closeBtn =>{
 
 
 // ......All Function will go here undernith...............
-// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-
-
-
-
-
-
-
-
-
-
-//All code will be deleted here.............
-function printOut(text){
-    const div = document.createElement('div')
-    div.innerHTML = text
-    document.body.appendChild(div)
+ function activeModal(aModal){
+     aModal.classList.add('active')
+     overlay.classList.add('active')
+ }
+function clossModal(cBtn){
+ cBtn.classList.remove('active')
+  overlay.classList.remove('active')
 }
-// addEventListener('click',()=>{
-//     alert("this is working")
-// })
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
